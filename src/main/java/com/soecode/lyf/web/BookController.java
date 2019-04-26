4826bde571f8d4da42f49e0c07d20c931cd2ff21
lam-win10 <jxh_8899@126.com>
2019-04-26 14:43:06 +0800

    * 新增验证码

--- src/main/java/com/soecode/lyf/web/BookController.java
+++ src/main/java/com/soecode/lyf/web/BookController.java
@@ -1,19 +1,7 @@
 package com.soecode.lyf.web;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import com.octo.captcha.service.CaptchaServiceException;
+import com.octo.captcha.service.image.ImageCaptchaService;
 import com.soecode.lyf.dto.AppointExecution;
 import com.soecode.lyf.dto.Result;
 import com.soecode.lyf.entity.Book;
@@ -21,56 +9,122 @@ import com.soecode.lyf.enums.AppointStateEnum;
 import com.soecode.lyf.exception.NoNumberException;
 import com.soecode.lyf.exception.RepeatAppointException;
 import com.soecode.lyf.service.BookService;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/book") // url:/模块/资源/{id}/细分 /seckill/list
 public class BookController {
 
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	private BookService bookService;
+    @Autowired
+    private BookService bookService;
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	private String list(Model model) {
-		List<Book> list = bookService.getList();
-		model.addAttribute("list", list);
-		// list.jsp + model = ModelAndView
-		return "list";// WEB-INF/jsp/"list".jsp
-	}
+    @Autowired
+    private ImageCaptchaService imageCaptchaService;
 
-	@RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
-	private String detail(@PathVariable("bookId") Long bookId, Model model) {
-		if (bookId == null) {
-			return "redirect:/book/list";
-		}
-		Book book = bookService.getById(bookId);
-		if (book == null) {
-			return "forward:/book/list";
-		}
-		model.addAttribute("book", book);
-		return "detail";
-	}
+    @RequestMapping(value = "/captcha", method = RequestMethod.GET)
+    private void captcha(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        byte[] captchaChallengeAsJpeg = null;
+        // 输出jpg的字节流
+        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+        try {
+            // https://blog.csdn.net/gzyz_lsl/article/details/79528642
+            // https://blog.csdn.net/xiao190128/article/details/54907723
+            // get the session id that will identify the generated captcha.
+            // the same id must be used to validate the response, the session id
+            // is a good candidate!
 
-	// ajax json
-	@RequestMapping(value = "/{bookId}/appoint", method = RequestMethod.POST, produces = {
-			"application/json; charset=utf-8" })
-	@ResponseBody
-	private Result<AppointExecution> appoint(@PathVariable("bookId") Long bookId, @RequestParam("studentId") Long studentId) {
-		if (studentId == null || studentId.equals("")) {
-			return new Result<>(false, "学号不能为空");
-		}
-		AppointExecution execution = null;
-		try {
-			execution = bookService.appoint(bookId, studentId);
-		} catch (NoNumberException e1) {
-			execution = new AppointExecution(bookId, AppointStateEnum.NO_NUMBER);
-		} catch (RepeatAppointException e2) {
-			execution = new AppointExecution(bookId, AppointStateEnum.REPEAT_APPOINT);
-		} catch (Exception e) {
-			execution = new AppointExecution(bookId, AppointStateEnum.INNER_ERROR);
-		}
-		return new Result<AppointExecution>(true, execution);
-	}
+            //String captchaId = session.getSessionId(request);
+
+            String captchaId = request.getSession().getId();
+            BufferedImage challenge = imageCaptchaService.getImageChallengeForID(
+                    captchaId, request.getLocale());
+            // a jpeg encoder
+            JPEGImageEncoder jpegEncoder = JPEGCodec
+                    .createJPEGEncoder(jpegOutputStream);
+            jpegEncoder.encode(challenge);
+        } catch (IllegalArgumentException e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        } catch (CaptchaServiceException e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+
+        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+
+        // flush it in the response
+        response.setHeader("Cache-Control", "no-store");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        //response.setContentType("image/" + CAPTCHA_IMAGE_FORMAT);
+        response.setContentType("image/jpeg");
+
+        ServletOutputStream responseOutputStream = response.getOutputStream();
+        responseOutputStream.write(captchaChallengeAsJpeg);
+        responseOutputStream.flush();
+
+        responseOutputStream.close();
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    private String list(Model model) {
+        List<Book> list = bookService.getList();
+        model.addAttribute("list", list);
+        // list.jsp + model = ModelAndView
+        return "list";// WEB-INF/jsp/"list".jsp
+    }
+
+    @RequestMapping(value = "/{bookId}/detail", method = RequestMethod.GET)
+    private String detail(@PathVariable("bookId") Long bookId, Model model) {
+        if (bookId == null) {
+            return "redirect:/book/list";
+        }
+        Book book = bookService.getById(bookId);
+        if (book == null) {
+            return "forward:/book/list";
+        }
+        model.addAttribute("book", book);
+        return "detail";
+    }
+
+    // ajax json
+    @RequestMapping(value = "/{bookId}/appoint", method = RequestMethod.POST, produces = {
+            "application/json; charset=utf-8"})
+    @ResponseBody
+    private Result<AppointExecution> appoint(@PathVariable("bookId") Long bookId, @RequestParam("studentId") Long studentId) {
+        if (studentId == null || studentId.equals("")) {
+            return new Result<>(false, "学号不能为空");
+        }
+        AppointExecution execution = null;
+        try {
+            execution = bookService.appoint(bookId, studentId);
+        } catch (NoNumberException e1) {
+            execution = new AppointExecution(bookId, AppointStateEnum.NO_NUMBER);
+        } catch (RepeatAppointException e2) {
+            execution = new AppointExecution(bookId, AppointStateEnum.REPEAT_APPOINT);
+        } catch (Exception e) {
+            execution = new AppointExecution(bookId, AppointStateEnum.INNER_ERROR);
+        }
+        return new Result<AppointExecution>(true, execution);
+    }
 
 }
